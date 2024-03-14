b63ddfc109d2efe25bf3ee6860fcd0504bd46467
ニシ <bunsekimal@gmail.com>
2024-03-14 22:34:40 +0900

    レイアウト完成

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>portfolio</title>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+    <body>
+        <header class="page-header wrapper">
+            <h1><a href="/">portfolio</a></h1>
+            <nav>
+                <ul class="main-nav">
+                    <li><a href="/login">ログイン</a></li>
+                    <li><a href="/register">新規登録</a></li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+
+    <footer>
+        <div class="wrapper">
+            <p><small>&copy; 2024 Nishi</small></p>
+        </div>
+    </footer>
+</html>
