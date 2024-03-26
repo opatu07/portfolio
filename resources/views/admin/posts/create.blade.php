6d1f30afee602b4e390c6b8739ebed02a9fbbec2
ニシ <bunsekimal@gmail.com>
2024-03-26 17:45:17 +0900

    新規登録画面のフォームを修正

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,14 +3,50 @@
         <form method="POST" action="/admin/posts" enctype="multipart/form-data">
             @csrf
 
-            <x-form.input name="title" required/>
-            <x-form.input name="slug" required/>
-            <x-form.input name="thumbnail" type="file" required/>
-            <x-form.textarea name="excerpt" required/>
-            <x-form.textarea name="body" required/>
+            <div class="py-2">
+                <x-form.label name="タイトル"/>  
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                        name="title"
+                        id="title"
+                    >
+                <x-form.error name="title"/>
+            </div>
+            <div class="py-2">
+                <x-form.label name="スラッグ"/>  
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                        name="slug"
+                        id="slug"
+                    >
+                <x-form.error name="slug"/>
+            </div>
+            <div class="py-2">
+                <x-form.label name="サムネイル"/>  
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                        name="thumbnail"
+                        id="thumbnail"
+                        type="file"
+                    >
+                <x-form.error name="thumbnail"/>
+            </div>
+            <div class="py-2">
+                <x-form.label name="要約"/>  
+                    <textarea class="border border-gray-200 p-2 w-full rounded"
+                        name="excerpt"
+                        id="excerpt"
+                    ></textarea>
+                <x-form.error name="excerpt"/>
+            </div>
+            <div class="py-2">
+                <x-form.label name="内容"/>  
+                    <textarea class="border border-gray-200 p-2 w-full rounded"
+                        name="body"
+                        id="body"
+                    ></textarea>
+                <x-form.error name="body"/>
+            </div>
 
             <x-form.field>
-                <x-form.label name="category"/>
+                <x-form.label name="カテゴリー"/>
 
                 <select name="category_id" id="category_id" required>
                     @foreach (\App\Models\Category::all() as $category)
