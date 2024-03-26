80bce15769d89a3bc46d1b09196ead4d081804c8
ニシ <bunsekimal@gmail.com>
2024-03-26 21:29:44 +0900

    記事の編集画面のフォームを修正

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -1,25 +1,63 @@
 <x-layout>
-    <x-setting :heading="'Edit Post: ' . $post->title">
+    <x-setting :heading="'記事を編集する: ' . $post->title">
         <form method="POST" action="/admin/posts/{{ $post->id }}" enctype="multipart/form-data">
             @csrf
             @method('PATCH')
 
-            <x-form.input name="title" :value="old('title', $post->title)" required />
-            <x-form.input name="slug" :value="old('slug', $post->slug)" required />
+            <div class="py-2">
+                <x-form.label name="タイトル"/>  
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                        name="title"
+                        id="title"
+                        value="{{ old('title', $post->title) }}"
+                    >
+                <x-form.error name="title"/>
+            </div>
+
+            <div class="py-2">
+                <x-form.label name="スラッグ"/>  
+                    <input class="border border-gray-200 p-2 w-full rounded"
+                        name="slug"
+                        id="slug"
+                        value="{{ old('slug', $post->slug) }}"
+                    >
+                <x-form.error name="slug"/>
+            </div>
 
             <div class="flex mt-6">
                 <div class="flex-1">
-                    <x-form.input name="thumbnail" type="file" :value="old('thumbnail', $post->thumbnail)" />
+                    <x-form.label name="サムネイル"/>  
+                        <input class="border border-gray-200 p-2 w-full rounded"
+                            name="thumbnail"
+                            id="thumbnail"
+                            type="file"
+                            value="{{ old('thumbnail', $post->thumbnail) }}"
+                        >
+                    <x-form.error name="thumbnail"/>
                 </div>
-
                 <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl ml-6" width="100">
             </div>
 
-            <x-form.textarea name="excerpt" required>{{ old('excerpt', $post->excerpt) }}</x-form.textarea>
-            <x-form.textarea name="body" required>{{ old('body', $post->body) }}</x-form.textarea>
+            <div class="py-2">
+                <x-form.label name="要約"/>  
+                    <textarea class="border border-gray-200 p-2 w-full rounded"
+                        name="excerpt"
+                        id="excerpt"
+                    >{{ old('excerpt', $post->excerpt) }}</textarea>
+                <x-form.error name="excerpt"/>
+            </div>
+
+            <div class="py-2">
+                <x-form.label name="内容"/>  
+                    <textarea class="border border-gray-200 p-2 w-full rounded"
+                        name="body"
+                        id="body"
+                    >{{ old('body', $post->body) }}</textarea>
+                <x-form.error name="body"/>
+            </div>
 
             <x-form.field>
-                <x-form.label name="category"/>
+                <x-form.label name="カテゴリー"/>
 
                 <select name="category_id" id="category_id" required>
                     @foreach (\App\Models\Category::all() as $category)
