80d71f013d2c10a17eeb9f9e4c078f1550eb59aa
ニシ <bunsekimal@gmail.com>
2024-03-27 12:15:16 +0900

    パスワードリセットを修正

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,23 +1,37 @@
 <x-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+    <section class="px-6 py-8">
+        <main class="w-3/4 mx-auto mt-10">
+            <x-panel>
+                <h1 class="text-center font-bold text-xl">パスワードリセット画面</h1>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+                <div class="mb-4 text-sm py-2 text-gray-600 dark:text-gray-400">
+                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                </div>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+                <!-- Session Status -->
+                <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Email Address -->
-        <div>
-            <x-form.input name="email" type="email" required />
-        </div>
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
 
-        <div class="flex items-center justify-end mt-4">
-            <x-form.button>
-                {{ __('Email Password Reset Link') }}
-            </x-form.button>
-        </div>
-    </form>
+                    <!-- Email Address -->
+                    <div>
+                        <x-form.label name="メールアドレス"/>  
+                        <input class="border border-gray-200 p-2 w-full rounded"
+                            name="email"
+                            id="email"
+                            type="email"
+                        >
+                        <x-form.error name="email"/>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <x-form.button>
+                            {{ __('Email Password Reset Link') }}
+                        </x-form.button>
+                    </div>
+                </form>
+            </x-panel>
+        </main>
+    </section>
 </x-layout>
