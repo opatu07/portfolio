7c7190d22cf6746067e13e1c8aa93add3c109a42
ニシ <bunsekimal@gmail.com>
2024-03-29 10:25:15 +0900

    投稿記事一覧と上の余白部分のレスポンシブ対応

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -1,5 +1,5 @@
-<header class="max-w-xl mx-auto mt-20 text-center">
-    <h1 class="md:text-4xl">
+<header class="max-w-xl mx-auto md:mt-20 mt-10 text-center">
+    <h1 class="md:text-4xl text-2xl">
         投稿記事一覧
     </h1>
 
