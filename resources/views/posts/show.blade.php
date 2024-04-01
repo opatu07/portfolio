c482d9933607a32df74e5d243cf3f4edb6d7392a
ニシ <bunsekimal@gmail.com>
2024-04-02 00:08:07 +0900

    詳細ページのレイアウトを編集

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,7 +2,8 @@
     <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
         <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
             <div class="col-span-4 lg:text-center lg:pt-14 mb-10">
-                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl" width="20" height="20"> 
+               
+                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="" class="rounded-xl" width="600" height="150"> 
 
                 <div class="flex items-center lg:justify-center text-sm mt-4">
                     <div class="ml-3 text-left">
