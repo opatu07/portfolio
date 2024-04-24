999068a12a13bc745323ad202c6323a50d673601
ニシ <bunsekimal@gmail.com>
2024-04-24 09:59:34 +0900

    セキュリティの強化

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -25,8 +25,12 @@ class ProfileController extends Controller
      * Update the user's profile information.
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
-    {
-        $request->user()->fill($request->validated());
+    {   
+        $request->validate([
+            'username' => 'required|unique:users,username|min:3|max:20',
+        ]);
+
+        $request->user()->fill($request);
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
